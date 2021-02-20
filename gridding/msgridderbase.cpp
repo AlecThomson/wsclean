80b6a2aa936695af7d37cf0f964f620762f97784
André Offringa <offringa@gmail.com>
2021-02-20 21:16:48 +0000

    Fix psf rotation

--- gridding/msgridderbase.cpp
+++ gridding/msgridderbase.cpp
@@ -401,7 +401,7 @@ void MSGridderBase::readAndWeightVisibilities(MSProvider& msProvider,
   const std::size_t dataSize = curBand.ChannelCount() * PolarizationCount;
   if (DoImagePSF()) {
     std::fill_n(rowData.data, dataSize, 1.0);
-    if (HasDenormalPhaseCentre()) {
+    if (HasDenormalPhaseCentre() && _settings.facetRegionFilename.empty()) {
       const double lmsqrt = std::sqrt(1.0 - PhaseCentreDL() * PhaseCentreDL() -
                                       PhaseCentreDM() * PhaseCentreDM());
       const double shiftFactor = 2.0 * M_PI *
