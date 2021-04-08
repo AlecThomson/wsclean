11b49510d34fe1bca99cecc6c5513ddd9957ba98
Jakob Maljaars <jakob.maljaars@stcorp.nl>
2021-04-08 16:14:29 +0200

    Install casacore-tools

--- tests/facets/tPredict.sh
+++ tests/facets/tPredict.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Runs a predict on a provided model image with and without faceting
 # Script is assumed to be run from within the build/ directory
 
@@ -39,17 +41,9 @@ wsclean ${common} ${MWA_MOCK_FULL}
 echo "===== Predicting facet ====="
 wsclean ${common} -facet-regions ${facetfile} ${MWA_MOCK_FACET}
 
+# taql requires casacore(-tools)
+taql "select from MWA_MOCK_FULL.ms t1, MWA_MOCK_FACET.ms t2 where not all(near(t1.MODEL_DATA,t2.MODEL_DATA,3e-3))" > taql.out
 
-if ! command -v taql &> /dev/null
-then
-  echo "taql could not be found, install DP3 to run this check"
-  exit 1
-else
-  # DP3 needed for taql
-  taql "select from MWA_MOCK_FULL.ms t1, MWA_MOCK_FACET.ms t2 where not all(near(t1.MODEL_DATA,t2.MODEL_DATA,3e-3))" > taql.out
-
-  # Create expected taql output.
-  echo "    select result of 0 rows" > taql.ref
-  diff taql.out taql.ref  ||  exit 1
-  echo "Succes!"
-fi
+# Create expected taql output.
+echo "    select result of 0 rows" > taql.ref
+diff taql.out taql.ref  ||  exit 1
