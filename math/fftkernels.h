e0304dd07283730c1dd1120add64ca2b6bd5e26a
Bram Veenboer <bram.veenboer@gmail.com>
2021-07-16 08:37:04 +0200

    Parellelize fft2f_r2c_composite and fft2f_c2r_composite

--- math/fftkernels.h
+++ math/fftkernels.h
@@ -4,9 +4,10 @@
 #include <fftw3.h>
 
 void fft2f_r2c_composite(size_t imgHeight, size_t imgWidth, const float *in,
-                         fftwf_complex *out);
+                         fftwf_complex *out, size_t threadCount = 1);
 
 void fft2f_c2r_composite(size_t imgHeight, size_t imgWidth,
-                         const fftwf_complex *in, float *out);
+                         const fftwf_complex *in, float *out,
+                         size_t threadCount = 1);
 
 #endif
