f881db153122108a324c2e543c67ffcb53bd88a0
Bram Veenboer <bram.veenboer@gmail.com>
2021-07-26 11:06:43 +0200

    Use std::copy_n in fft2f_r2c_composite

--- math/fftkernels.cpp
+++ math/fftkernels.cpp
@@ -1,6 +1,7 @@
 #include "fftkernels.h"
 
 #include <cstring>
+#include <algorithm>
 
 void fft2f_r2c_composite(fftwf_plan plan_r2c, fftwf_plan plan_c2c,
                          size_t imgHeight, size_t imgWidth, const float *in,
@@ -13,11 +14,12 @@ void fft2f_r2c_composite(fftwf_plan plan_r2c, fftwf_plan plan_c2c,
 
   loop.Run(0, imgHeight, [&](size_t yStart, size_t yEnd) {
     fftwf_complex *temp2 = fftwf_alloc_complex(complexWidth);
+    float *temp2_ptr = reinterpret_cast<float *>(temp2);
     for (size_t y = yStart; y < yEnd; y++) {
-      memcpy(temp2, &in[y * imgWidth], imgWidth * sizeof(float));
-      fftwf_execute_dft_r2c(plan_r2c, reinterpret_cast<float *>(temp2), temp2);
-      memcpy(&temp1[y * complexWidth], temp2,
-             complexWidth * sizeof(fftwf_complex));
+      float *temp1_ptr = reinterpret_cast<float *>(&temp1[y * complexWidth]);
+      std::copy_n(&in[y * imgWidth], imgWidth, temp2_ptr);
+      fftwf_execute_dft_r2c(plan_r2c, temp2_ptr, temp2);
+      std::copy_n(temp2_ptr, 2 * complexWidth, temp1_ptr);
     }
     fftwf_free(temp2);
   });
@@ -28,7 +30,10 @@ void fft2f_r2c_composite(fftwf_plan plan_r2c, fftwf_plan plan_c2c,
     for (size_t x = xStart; x < xEnd; x++) {
       // Copy input
       for (size_t y = 0; y < imgHeight; y++) {
-        memcpy(&temp2[y], &temp1[y * complexWidth + x], sizeof(fftwf_complex));
+        float *temp1_ptr =
+            reinterpret_cast<float *>(&temp1[y * complexWidth + x]);
+        float *temp2_ptr = reinterpret_cast<float *>(&temp2[y]);
+        std::copy_n(temp1_ptr, 2, temp2_ptr);
       }
 
       // Perform 1D FFT over column
@@ -36,7 +41,9 @@ void fft2f_r2c_composite(fftwf_plan plan_r2c, fftwf_plan plan_c2c,
 
       // Transpose output
       for (size_t y = 0; y < imgHeight; y++) {
-        memcpy(&out[y * complexWidth + x], temp2[y], sizeof(fftwf_complex));
+        float *temp2_ptr = reinterpret_cast<float *>(&temp2[y]);
+        float *out_ptr = reinterpret_cast<float *>(&out[y * complexWidth + x]);
+        std::copy_n(temp2_ptr, 2, out_ptr);
       }
     }
 
