336cdbe3ebdc8a5aada208306cbfbded8c985f58
Bram Veenboer <bram.veenboer@gmail.com>
2021-07-29 09:00:51 +0200

    Partially unroll first part of fft2f_c2r_composite
    This required the helper function round_up, because FFTW apparantly
    assumes aligned data. Without padding, the code produces a segfault.

--- math/fftkernels.cpp
+++ math/fftkernels.cpp
@@ -3,6 +3,8 @@
 #include <cstring>
 #include <algorithm>
 
+size_t round_up(size_t a, size_t b) { return ((a + b) / b) * b; }
+
 void fft2f_r2c_composite(fftwf_plan plan_r2c, fftwf_plan plan_c2c,
                          size_t imgHeight, size_t imgWidth, const float *in,
                          fftwf_complex *out,
@@ -105,21 +107,40 @@ void fft2f_c2r_composite(fftwf_plan plan_c2c, fftwf_plan plan_c2r,
   });
 
   loop.Run(0, imgHeight, [&](size_t yStart, size_t yEnd) {
-    fftwf_complex *temp2 = fftwf_alloc_complex(complexWidth);
-    for (size_t y = yStart; y < yEnd; y++) {
+    size_t unroll = 4;
+    size_t paddedWidth = round_up(complexWidth, 64);
+    fftwf_complex *temp2 = fftwf_alloc_complex(unroll * paddedWidth);
+
+    for (size_t y = yStart; y < yEnd; y += unroll) {
       // Transpose input
       for (size_t x = 0; x < complexWidth; x++) {
-        float *temp1_ptr = reinterpret_cast<float *>(&temp1[x * imgHeight + y]);
-        float *temp2_ptr = reinterpret_cast<float *>(&temp2[x]);
-        std::copy_n(temp1_ptr, 2, temp2_ptr);
+        for (size_t i = 0; i < unroll; i++) {
+          if ((y + i) < yEnd) {
+            float *temp1_ptr =
+                reinterpret_cast<float *>(&temp1[x * imgHeight + y + i]);
+            float *temp2_ptr =
+                reinterpret_cast<float *>(&temp2[i * paddedWidth + x]);
+            std::copy_n(temp1_ptr, 2, temp2_ptr);
+          }
+        }
       }
 
-      // Perform 1D C2R FFT over row
-      fftwf_execute_dft_c2r(plan_c2r, temp2, reinterpret_cast<float *>(temp2));
+      // Perform 1D C2R FFT over rows
+      for (size_t i = 0; i < unroll; i++) {
+        if ((y + i) < yEnd) {
+          fftwf_complex *temp2_ptr = &temp2[i * paddedWidth];
+          fftwf_execute_dft_c2r(plan_c2r, temp2_ptr,
+                                reinterpret_cast<float *>(temp2_ptr));
+        }
+      }
 
       // Copy output
-      float *temp2_ptr = reinterpret_cast<float *>(temp2);
-      std::copy_n(temp2_ptr, imgWidth, &out[y * imgWidth]);
+      for (size_t i = 0; i < unroll; i++) {
+        if ((y + i) < yEnd) {
+          float *temp2_ptr = reinterpret_cast<float *>(&temp2[i * paddedWidth]);
+          std::copy_n(temp2_ptr, imgWidth, &out[(y + i) * imgWidth]);
+        }
+      }
     }
 
     fftwf_free(temp2);
