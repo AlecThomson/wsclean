f893ac565274c5e39e2b28901995db533a6e60cb
Bram Veenboer <bram.veenboer@gmail.com>
2021-08-16 15:14:08 +0200

    Replace defines by constexpr in fftkernels

--- math/fftkernels.cpp
+++ math/fftkernels.cpp
@@ -3,14 +3,14 @@
 #include <cstring>
 #include <algorithm>
 
-// Partially unroll rows/columns with a factor of UNROLL
-#define UNROLL 4
-
-// With UNROLL > 1, the temporary buffers need to be aligned
-// for FFTW to work correctly.
-#define ALIGNMENT 64
-
 namespace {
+
+// Partially unroll rows/columns with a factor of kUnroll
+constexpr size_t kUnroll = 4;
+
+// With kUnroll > 1, the temporary buffers need to be aligned
+// for FFTW to work correctly.
+constexpr size_t kAlignment = 64;
 
 size_t roundUp(size_t a, size_t b) { return ((a + b) / b) * b; }
 
@@ -38,14 +38,14 @@ void fft2f_r2c_composite(fftwf_plan plan_r2c, fftwf_plan plan_c2c,
   });
 
   loop.Run(0, complexWidth, [&](size_t xStart, size_t xEnd) {
-    // Partially UNROLL over columns
-    size_t paddedHeight = roundUp(imgHeight, ALIGNMENT);
-    fftwf_complex *temp2 = fftwf_alloc_complex(UNROLL * paddedHeight);
+    // Partially kUnroll over columns
+    size_t paddedHeight = roundUp(imgHeight, kAlignment);
+    fftwf_complex *temp2 = fftwf_alloc_complex(kUnroll * paddedHeight);
 
-    for (size_t x = xStart; x < xEnd; x += UNROLL) {
+    for (size_t x = xStart; x < xEnd; x += kUnroll) {
       // Copy input
       for (size_t y = 0; y < imgHeight; y++) {
-        for (size_t i = 0; i < UNROLL; i++) {
+        for (size_t i = 0; i < kUnroll; i++) {
           if ((x + i) < xEnd) {
             float *temp1_ptr =
                 reinterpret_cast<float *>(&temp1[y * complexWidth + x + i]);
@@ -57,14 +57,14 @@ void fft2f_r2c_composite(fftwf_plan plan_r2c, fftwf_plan plan_c2c,
       }
 
       // Perform 1D FFT over columns
-      for (size_t i = 0; i < UNROLL; i++) {
+      for (size_t i = 0; i < kUnroll; i++) {
         fftwf_complex *temp2_ptr = &temp2[i * paddedHeight];
         fftwf_execute_dft(plan_c2c, temp2_ptr, temp2_ptr);
       }
 
       // Transpose output
       for (size_t y = 0; y < imgHeight; y++) {
-        for (size_t i = 0; i < UNROLL; i++) {
+        for (size_t i = 0; i < kUnroll; i++) {
           if ((x + i) < xEnd) {
             float *temp2_ptr =
                 reinterpret_cast<float *>(&temp2[i * paddedHeight + y]);
@@ -88,15 +88,15 @@ void fft2f_c2r_composite(fftwf_plan plan_c2c, fftwf_plan plan_c2r,
                          aocommon::StaticFor<size_t> &loop) {
   const size_t complexWidth = imgWidth / 2 + 1;
 
-  size_t paddedHeight = roundUp(imgHeight, ALIGNMENT);
+  size_t paddedHeight = roundUp(imgHeight, kAlignment);
   size_t paddedSize = paddedHeight * complexWidth;
   fftwf_complex *temp1 = fftwf_alloc_complex(paddedSize);
 
   loop.Run(0, complexWidth, [&](size_t xStart, size_t xEnd) {
-    for (size_t x = xStart; x < xEnd; x += UNROLL) {
+    for (size_t x = xStart; x < xEnd; x += kUnroll) {
       // Transpose input
       for (size_t y = 0; y < imgHeight; y++) {
-        for (size_t i = 0; i < UNROLL; i++) {
+        for (size_t i = 0; i < kUnroll; i++) {
           if ((x + i) < xEnd) {
             const float *in_ptr =
                 reinterpret_cast<const float *>(&in[y * complexWidth + x + i]);
@@ -108,7 +108,7 @@ void fft2f_c2r_composite(fftwf_plan plan_c2c, fftwf_plan plan_c2r,
       }
 
       // Perform 1D C2C FFT over columns
-      for (size_t i = 0; i < UNROLL; i++) {
+      for (size_t i = 0; i < kUnroll; i++) {
         if ((x + i) < xEnd) {
           fftwf_complex *temp1_ptr = &temp1[(x + i) * paddedHeight];
           fftwf_execute_dft(plan_c2c, temp1_ptr, temp1_ptr);
@@ -118,13 +118,13 @@ void fft2f_c2r_composite(fftwf_plan plan_c2c, fftwf_plan plan_c2r,
   });
 
   loop.Run(0, imgHeight, [&](size_t yStart, size_t yEnd) {
-    size_t paddedWidth = roundUp(complexWidth, ALIGNMENT);
-    fftwf_complex *temp2 = fftwf_alloc_complex(UNROLL * paddedWidth);
+    size_t paddedWidth = roundUp(complexWidth, kAlignment);
+    fftwf_complex *temp2 = fftwf_alloc_complex(kUnroll * paddedWidth);
 
-    for (size_t y = yStart; y < yEnd; y += UNROLL) {
+    for (size_t y = yStart; y < yEnd; y += kUnroll) {
       // Transpose input
       for (size_t x = 0; x < complexWidth; x++) {
-        for (size_t i = 0; i < UNROLL; i++) {
+        for (size_t i = 0; i < kUnroll; i++) {
           if ((y + i) < yEnd) {
             float *temp1_ptr =
                 reinterpret_cast<float *>(&temp1[x * paddedHeight + y + i]);
@@ -136,7 +136,7 @@ void fft2f_c2r_composite(fftwf_plan plan_c2c, fftwf_plan plan_c2r,
       }
 
       // Perform 1D C2R FFT over rows
-      for (size_t i = 0; i < UNROLL; i++) {
+      for (size_t i = 0; i < kUnroll; i++) {
         if ((y + i) < yEnd) {
           fftwf_complex *temp2_ptr = &temp2[i * paddedWidth];
           fftwf_execute_dft_c2r(plan_c2r, temp2_ptr,
@@ -145,7 +145,7 @@ void fft2f_c2r_composite(fftwf_plan plan_c2c, fftwf_plan plan_c2r,
       }
 
       // Copy output
-      for (size_t i = 0; i < UNROLL; i++) {
+      for (size_t i = 0; i < kUnroll; i++) {
         if ((y + i) < yEnd) {
           float *temp2_ptr = reinterpret_cast<float *>(&temp2[i * paddedWidth]);
           std::copy_n(temp2_ptr, imgWidth, &out[(y + i) * imgWidth]);
