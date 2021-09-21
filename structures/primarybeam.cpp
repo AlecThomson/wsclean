2316d947dd1ea926d1e1d0494119b38ec095b5a8
Jakob Maljaars <jakob.maljaars@stcorp.nl>
2021-09-21 12:22:04 +0200

    Deprecate pb-undersampling and introduce pb-grid-size

--- structures/primarybeam.cpp
+++ structures/primarybeam.cpp
@@ -55,11 +55,9 @@ PrimaryBeam::PrimaryBeam(const Settings& settings)
       _phaseCentreRA(0.0),
       _phaseCentreDec(0.0),
       _phaseCentreDL(0.0),
-      _phaseCentreDM(0.0) {
-  // Undersampling factor (default 8) and beam update time (default 1800s), see
-  // settings.h
-  _undersample = _settings.primaryBeamUndersampling;
-  _secondsBeforeBeamUpdate = _settings.primaryBeamUpdateTime;
+      _phaseCentreDM(0.0),
+      _undersample(computeUndersamplingFactor()),
+      _secondsBeforeBeamUpdate(_settings.primaryBeamUpdateTime) {
 }
 
 PrimaryBeam::~PrimaryBeam() {}
@@ -537,3 +535,15 @@ std::tuple<double, double, size_t> PrimaryBeam::GetTimeInfo(
   return std::make_tuple(startTime, endTime, intervalCount);
 }
 #endif  // HAVE_EVERYBEAM
+
+size_t PrimaryBeam::computeUndersamplingFactor() const {
+  const size_t undersample =
+      std::min(_settings.trimmedImageWidth / _settings.primaryBeamGridSize,
+               _settings.trimmedImageHeight / _settings.primaryBeamGridSize);
+  if (undersample == 0) {
+    throw std::runtime_error(
+        "The chosen primary beam grid size is larger than the shortest side of "
+        "the image. Please decrease the primary beam grid size.");
+  }
+  return undersample;
+}
