9a584a011aa2475f8a4a85593d877c3d89b021ae
Maik Nijhuis <maik.nijhuis@triopsys.nl>
2022-07-06 14:10:12 +0200

    Add ddpsf in structures

---
+++ structures/ddpsf.cpp
@@ -0,0 +1,51 @@
+#include "ddpsf.h"
+
+#include <aocommon/imagecoordinates.h>
+
+std::vector<schaapcommon::facets::Facet> CreateRectangularPsfs(
+    const double phase_centre_ra, const double phase_centre_dec,
+    const double pixelScaleX, const double pixel_scale_y,
+    const double trimmed_image_height, const double trimmed_image_width,
+    const size_t psf_grid_width, const size_t psf_grid_height) {
+  std::vector<schaapcommon::facets::Facet> ddpsfs;
+  ddpsfs.reserve(psf_grid_height * psf_grid_width);
+
+  double single_psf_height = trimmed_image_height / psf_grid_height;
+  double single_psf_width = trimmed_image_width / psf_grid_width;
+
+  for (size_t grid_y = 0; grid_y < psf_grid_height; ++grid_y) {
+    for (size_t grid_x = 0; grid_x < psf_grid_width; ++grid_x) {
+      schaapcommon::facets::Facet facet;
+
+      const auto add_vertex = [&](size_t x, size_t y) {
+        double l = 0.0;
+        double m = 0.0;
+        double ra = 0.0;
+        double dec = 0.0;
+        aocommon::ImageCoordinates::XYToLM(x, y, pixelScaleX, pixel_scale_y,
+                                           trimmed_image_width,
+                                           trimmed_image_height, l, m);
+        aocommon::ImageCoordinates::LMToRaDec(l, m, phase_centre_ra,
+                                              phase_centre_dec, ra, dec);
+        facet.AddVertex(ra, dec);
+      };
+
+      const size_t facet_start_x = grid_x * single_psf_width;
+      const size_t facet_start_y = grid_y * single_psf_height;
+      const size_t facet_end_x = (grid_x + 1) * single_psf_width;
+      const size_t facet_end_y = (grid_y + 1) * single_psf_height;
+      add_vertex(facet_start_x, facet_start_y);
+      add_vertex(facet_start_x, facet_end_y);
+      add_vertex(facet_end_x, facet_end_y);
+      add_vertex(facet_end_x, facet_start_y);
+
+      // add a name label for this box
+      facet.SetDirectionLabel(std::to_string(grid_y) + ", " +
+                              std::to_string(grid_x));
+
+      ddpsfs.push_back(std::move(facet));
+    }
+  }
+
+  return ddpsfs;
+}
---
+++ structures/ddpsf.h
@@ -0,0 +1,13 @@
+#ifndef STRUCTURES_DDPSF_H_
+#define STRUCTURES_DDPSF_H_
+
+#include <vector>
+
+#include <schaapcommon/facets/facet.h>
+
+std::vector<schaapcommon::facets::Facet> CreateRectangularPsfs(
+    double phase_centre_ra, double phase_centre_dec, double pixel_scale_x,
+    double pixel_scale_y, double trimmed_image_height,
+    double trimmed_image_width, size_t psf_grid_width, size_t psf_grid_height);
+
+#endif  // STRUCTURES_DDPSF_H_
