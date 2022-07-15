fa9f75a99f97dac866b576e2fefa499f84504d1b
Maik Nijhuis <maik.nijhuis@triopsys.nl>
2022-07-15 13:01:14 +0200

    Check bounding boxes in facet util test

--- tests/structures/tfacetutil.cpp
+++ tests/structures/tfacetutil.cpp
@@ -2,26 +2,54 @@
 
 #include <boost/test/unit_test.hpp>
 
+using schaapcommon::facets::BoundingBox;
 using schaapcommon::facets::Facet;
 
-BOOST_AUTO_TEST_SUITE(dd_psf)
+BOOST_AUTO_TEST_SUITE(facet_util)
 
-BOOST_AUTO_TEST_CASE(create_single_psf) {
-  Facet::InitializationData facet_data(0.01, 100);
+BOOST_AUTO_TEST_CASE(create_grid_single_facet) {
+  const size_t kImageWidth = 100;
+  const size_t kImageHeight = 142;
+  Facet::InitializationData facet_data(0.01, 0.01, kImageWidth, kImageHeight);
 
-  const std::vector<schaapcommon::facets::Facet> dd_psfs =
+  const std::vector<schaapcommon::facets::Facet> facets =
       CreateFacetGrid(facet_data, 1, 1);
 
-  BOOST_CHECK_EQUAL(dd_psfs.size(), 1);
+  BOOST_TEST_REQUIRE(facets.size() == 1);
+  const BoundingBox& box = facets.front().GetTrimmedBoundingBox();
+  BOOST_TEST(box.Min().x == 0);
+  BOOST_TEST(box.Min().y == 0);
+  BOOST_TEST(box.Max().x == kImageWidth);
+  BOOST_TEST(box.Max().y == kImageHeight);
 }
 
-BOOST_AUTO_TEST_CASE(create_multiple_psfs) {
-  Facet::InitializationData facet_data(0.01, 100);
+BOOST_AUTO_TEST_CASE(create_grid_multiple_facets) {
+  const size_t kImageSize = 100;
+  const size_t kGridWidth = 4;
+  const size_t kGridHeight = 5;
+  Facet::InitializationData facet_data(0.01, kImageSize);
+  const std::vector<schaapcommon::facets::Facet> facets =
+      CreateFacetGrid(facet_data, kGridWidth, kGridHeight);
 
-  const std::vector<schaapcommon::facets::Facet> dd_psfs =
-      CreateFacetGrid(facet_data, 5, 5);
+  BOOST_TEST(facets.size() == kGridWidth * kGridHeight);
 
-  BOOST_REQUIRE_EQUAL(dd_psfs.size(), 25);
+  std::set<std::pair<int, int>> grid_cells;
+  for (const Facet& facet : facets) {
+    const BoundingBox& box = facet.GetTrimmedBoundingBox();
+    BOOST_TEST(box.Width() == kImageSize / kGridWidth);
+    BOOST_TEST(box.Height() == kImageSize / kGridHeight);
+
+    const int grid_x = box.Centre().x / box.Width();
+    const int grid_y = box.Centre().y / box.Height();
+    BOOST_TEST(grid_x >= 0);
+    BOOST_TEST(grid_y >= 0);
+    BOOST_TEST(grid_x < static_cast<int>(kGridWidth));
+    BOOST_TEST(grid_y < static_cast<int>(kGridHeight));
+    grid_cells.emplace(grid_x, grid_y);
+  }
+
+  // Check that the generated grid contains all grid cells.
+  BOOST_TEST(grid_cells.size() == kGridWidth * kGridHeight);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
