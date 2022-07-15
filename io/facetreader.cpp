6a4f7c92f6aee50be1b9ea89d8d164a52e78ab59
Sebastiaan van der Tol <tol@astron.nl>
2022-07-15 20:35:16 +0200

    Fixes after rebase onto AST-967 Use new Facet reading interface

--- io/facetreader.cpp
+++ io/facetreader.cpp
@@ -9,17 +9,24 @@ using schaapcommon::facets::DS9FacetFile;
 using schaapcommon::facets::Facet;
 
 std::vector<std::shared_ptr<Facet>> FacetReader::ReadFacets(
-    const Settings& settings, const ObservationInfo& observation_info) {
-  const Facet::InitializationData data =
-      CreateFacetInitializationData(settings, observation_info);
+    std::string filename, double width, double height, double pixelScaleX,
+    double pixelScaleY, double phaseCentreRA, double phaseCentreDec,
+    double shiftL, double shiftM, double imagePadding, bool make_square) {
+  Facet::InitializationData data(pixelScaleX, pixelScaleY, width, height);
+  data.phase_centre.ra = phaseCentreRA;
+  data.phase_centre.dec = phaseCentreDec;
+  data.shift_l = shiftL;
+  data.shift_m = shiftM;
+  data.padding = imagePadding;
+  data.align = 2;
+  data.make_square = make_square;
 
   std::vector<std::shared_ptr<Facet>> facets;
-  if (!settings.facetRegionFilename.empty()) {
-    facets = DS9FacetFile(settings.facetRegionFilename).ReadShared(data);
+  if (!filename.empty()) {
+    facets = DS9FacetFile(filename).ReadShared(data);
 
     if (facets.empty()) {
-      throw std::runtime_error("No facets found in " +
-                               settings.facetRegionFilename);
+      throw std::runtime_error("No facets found in " + filename);
     }
   }
 
