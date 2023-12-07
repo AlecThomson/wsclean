d622153885ca2b5b79ff088098e46b314ba58e49
Martin Reinecke <martin.reinecke1@gmx.de>
2023-12-07 10:45:35 +0000

    Allow reverse ordered frequencies in wgridder

--- wgridder/wgriddinggridder_simple.cpp
+++ wgridder/wgriddinggridder_simple.cpp
@@ -47,8 +47,13 @@ void WGriddingGridder_Simple<NumT>::AddInversionData(
     size_t nrows, size_t nchan, const double *uvw, const double *freq,
     const std::complex<float> *vis) {
   cmav<double, 2> uvw2(uvw, {nrows, 3});
-  cmav<double, 1> freq2(freq, {nchan});
-  cmav<std::complex<float>, 2> ms(vis, {nrows, nchan});
+  bool decreasing_freq = (nchan > 1) && (freq[1] < freq[0]);
+  auto freq2(decreasing_freq ? cmav<double, 1>(freq + nchan - 1, {nchan}, {-1})
+                             : cmav<double, 1>(freq, {nchan}));
+  auto ms(decreasing_freq
+              ? cmav<std::complex<float>, 2>(vis + nchan - 1, {nrows, nchan},
+                                             {ptrdiff_t(nchan), -1})
+              : cmav<std::complex<float>, 2>(vis, {nrows, nchan}));
   vmav<NumT, 2> tdirty({width_t_, height_t_});
   cmav<float, 2> twgt(nullptr, {0, 0});
   cmav<std::uint8_t, 2> tmask(nullptr, {0, 0});
@@ -97,8 +102,13 @@ void WGriddingGridder_Simple<NumT>::PredictVisibilities(
     size_t nrows, size_t nchan, const double *uvw, const double *freq,
     std::complex<float> *vis) const {
   cmav<double, 2> uvw2(uvw, {nrows, 3});
-  cmav<double, 1> freq2(freq, {nchan});
-  vmav<std::complex<float>, 2> ms(vis, {nrows, nchan});
+  bool decreasing_freq = (nchan > 1) && (freq[1] < freq[0]);
+  auto freq2(decreasing_freq ? cmav<double, 1>(freq + nchan - 1, {nchan}, {-1})
+                             : cmav<double, 1>(freq, {nchan}));
+  auto ms(decreasing_freq
+              ? vmav<std::complex<float>, 2>(vis + nchan - 1, {nrows, nchan},
+                                             {ptrdiff_t(nchan), -1})
+              : vmav<std::complex<float>, 2>(vis, {nrows, nchan}));
   cmav<NumT, 2> tdirty(img.data(), {width_t_, height_t_});
   cmav<float, 2> twgt(nullptr, {0, 0});
   cmav<std::uint8_t, 2> tmask(nullptr, {0, 0});
