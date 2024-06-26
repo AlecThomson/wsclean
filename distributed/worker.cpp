9f55584b7606f3c32c1c569245a09102e62de8fa
Maik Nijhuis <maik.nijhuis@triopsys.nl>
2024-06-26 08:39:05 +0000

    AST-1511 Remove MPI writer locks

--- distributed/worker.cpp
+++ distributed/worker.cpp
@@ -20,8 +20,10 @@ void Worker::Run() {
   TaskMessage message;
   const size_t maximum_message_size =
       scheduler_.GetSettings().maxMpiMessageSize;
+
   do {
     MPI_Status status;
+
     aocommon::UVector<unsigned char> buffer(TaskMessage::kSerializedSize);
     MPI_Recv(buffer.data(), TaskMessage::kSerializedSize, MPI_BYTE, kMainNode,
              kTag, MPI_COMM_WORLD, &status);
@@ -29,9 +31,17 @@ void Worker::Run() {
     message.Unserialize(stream);
 
     switch (message.type) {
+      case TaskMessage::Type::kStart:
+        scheduler_.Start(message.n_writer_groups);
+        break;
+
+      case TaskMessage::Type::kFinish:
+        // The do..while loop will exit, too.
+        break;
+
       case TaskMessage::Type::kGriddingRequest: {
-        buffer.resize(message.bodySize);
-        MPI_Recv_Big(buffer.data(), message.bodySize, kMainNode, kTag,
+        buffer.resize(message.body_size);
+        MPI_Recv_Big(buffer.data(), message.body_size, kMainNode, kTag,
                      MPI_COMM_WORLD, &status, maximum_message_size);
         aocommon::SerialIStream stream(std::move(buffer));
         stream.UInt64();  // skip the nr of packages
@@ -41,9 +51,7 @@ void Worker::Run() {
         scheduler_.Run(std::move(task), [](GriddingResult&) {});
         break;
       }
-      case TaskMessage::Type::kLockGrant:
-        scheduler_.GrantLock(message.lockId);
-        break;
+
       default:
         aocommon::Logger::Warn
             << "wsclean-mp MPI worker received an unknown message type!\n";
